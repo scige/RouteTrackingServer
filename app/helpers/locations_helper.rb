9bdb741a39d46dd7a2b97eab54656a4fccc9e192
scige <panf06@gmail.com>
2013-11-24 18:35:31 +0800

    compute center in the map, and add some rails settings

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,15 @@
 module LocationsHelper
+  def compute_map_center_x(locations)
+    points = []
+    locations.each {|p| points << p.latitude}
+    points.sort!
+    (points[0] + points[-1]) / 2
+   end
+
+  def compute_map_center_y(locations)
+    points = []
+    locations.each {|p| points << p.longitude}
+    points.sort!
+    (points[0] + points[-1]) / 2
+  end
 end
